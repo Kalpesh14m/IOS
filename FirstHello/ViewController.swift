1b867d54be64f94275ad92326ae01410bb39a348
Kalpesh14m <kalpesh14m@gmail.com>
2017-03-04 04:10:01 -0800

    new program

--- FirstHello/ViewController.swift
+++ FirstHello/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //MARK: Properties
+    
+    @IBOutlet weak var displayLable: UILabel!
+    
+    @IBAction func SaySomethingTapped(sender: UIButton){
+    displayLable.text = "Hello Gaurav"
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Hello Gaurav")
     }
 
     override func didReceiveMemoryWarning() {
